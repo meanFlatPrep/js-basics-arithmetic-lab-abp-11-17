ade963733dabe2d43935e4ba61ad9a525647ca1a
mean pt <meanpt@gmail.com>
2017-11-12 20:14:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+
+const bi = 1000000000  ;
+let newID  =  oldID + bi  ;
+let ageIsValid = Number.isInteger(currentAge);
+let randomNumber = Math.floor (Math.random() * 1000000020) ;
